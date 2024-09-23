0161e0a52f71851aa00c880ab829f6e24783bc74
kasir <cengi@htldornbirn.at>
2024-09-23 08:15:41 +0200

    if bedingung 2 an Switch_statement finished

---
+++ src/Switch_statement.java
@@ -0,0 +1,33 @@
+import java.util.Random;
+
+public class Switch_statement {
+    public static void main(String[] args) {
+        Random random = new Random();
+        int randomNumber = random.nextInt(5, 10);
+        switch(randomNumber){
+            case 10:
+                System.out.println("Ten");
+                break;
+
+            case 9:
+                System.out.println("Nine");
+
+            case 8:
+                System.out.println("Eight");
+
+            case 7:
+                System.out.println("Seven");
+
+            case 6:
+                System.out.println("Six");
+
+            case 5:
+                System.out.println("Five");
+
+
+        }
+
+
+    }
+
+}
