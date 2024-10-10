10f7b550f264d82a63a24d498e7d235c6444f83a
kasir <cengi@htldornbirn.at>
2024-10-10 09:25:26 +0200

    arrays finished

--- src/Strings/Arrays.java
+++ src/Strings/Arrays.java
@@ -2,6 +2,28 @@ package Strings;
 
 public class Arrays {
     public static void main(String[] args) {
+    int [] throwsArrays = new int[6];
 
+    throwsArrays[4] = 9;
+
+
+        for (int i = 0 ; i < throwsArrays.length; i++){
+            System.out.print(throwsArrays[i] + " ");
+        }
+
+            int[][] playground= new int [3][3];
+
+        playground[0][0] = 1;
+        playground[2][1] = 2;
+
+        System.out.println("----------------");
+
+        for (int row = 0; row < playground.length; row ++) {
+            for (int col = 0; col < playground.length; col++){
+                System.out.print(playground[row][col] + " ");
+
+            }
+            System.out.println();
+        }
     }
 }
