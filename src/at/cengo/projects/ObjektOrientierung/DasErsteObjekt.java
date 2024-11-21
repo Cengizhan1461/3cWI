45a5d51d38eb32bfc37ce96a07e44e9d361dfcc6
kasir <cengi@htldornbirn.at>
2024-11-21 08:25:25 +0100

    getter setter finished

--- src/at/cengo/projects/ObjektOrientierung/DasErsteObjekt.java
+++ src/at/cengo/projects/ObjektOrientierung/DasErsteObjekt.java
@@ -1,34 +1,35 @@
 package at.cengo.projects.ObjektOrientierung;
+import at.cengo.projects.ObjektOrientierung.car;
 
 public class DasErsteObjekt {
     public static void main(String[] args) {
         int a = 7;
 
         car c1 = new car();
-        c1.brand = "Audi";
-        c1.fuelAmount= 50;
-        c1.fuelConsumption = 5;
-        c1.serialNumber= "A1234";
-        c1.colour= "Black";
-        c1.brake = "Ich Bremse";
-        c1.tankVolume = 55;
+        c1.setBrand("Audi");
+        c1.setFuelAmount(50);
+        c1.setFuelConsumption(5);
+        c1.setSerialNumber("A1234");
+        c1.setColour("Black");
+        c1.setBrake("Ich Bremse");
+        c1.setTankVolume(55);
 
 
-        System.out.println(c1.brand);
-        System.out.println(c1.fuelAmount);
-        System.out.println(c1.serialNumber);
-        System.out.println(c1.colour);
-        System.out.println(c1.brake);
-        System.out.println(c1.tankVolume);
+        System.out.println(c1.getBrake());
+        System.out.println(c1.getFuelAmount());
+        System.out.println(c1.getColour());
+        System.out.println(c1.getSerialNumber());
+        System.out.println(c1.getBrand());
+        System.out.println(c1.getTankVolume());
         c1.turboBoost();
         c1.honk(4);
         c1.getRemainingRange();
 
-
+/*
         car c3 = new car(17, "Opel", "1234O");
 
         System.out.println(c3.serialNumber);
-
+*/
 
 
     }
--- src/at/cengo/projects/ObjektOrientierung/car.java
+++ src/at/cengo/projects/ObjektOrientierung/car.java
@@ -3,20 +3,22 @@ package at.cengo.projects.ObjektOrientierung;
 public class car {
     // Instanz / Gedächtnis
 
-    public int fuelAmount;
-    public int fuelConsumption;
-    public String brand;
-    public String serialNumber;
-    public String colour;
-    public String brake;
-    public int tankVolume;
+    private int fuelAmount;
+    private int fuelConsumption;
+    private String brand;
+    private String serialNumber;
+    private String colour;
+    private String brake;
+    private int tankVolume;
 
+
+    /*
     public car(int fuelConsumption, String brand, String serialNumber){
                 this.fuelConsumption = fuelConsumption;
                 this.brand = brand;
                 this.serialNumber = serialNumber;
     }
-
+*/
 
 
     public void turboBoost() {
@@ -34,4 +36,67 @@ public class car {
     public void getRemainingRange() {
         System.out.println(fuelAmount / fuelConsumption);
     }
+
+
+    public void setFuelAmount(int fuelAmount) {
+        if (fuelAmount > 100){
+            this.fuelAmount = 100;
+        }else{
+            this.fuelAmount = fuelAmount;
+        }
+    }
+
+    public void setFuelConsumption(int fuelConsumption) {
+        this.fuelConsumption = fuelConsumption;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public void setBrake(String brake) {
+        this.brake = brake;
+    }
+
+    public void setColour(String colour) {
+        this.colour = colour;
+    }
+
+    public void setSerialNumber(String serialNumber) {
+        this.serialNumber = serialNumber;
+    }
+
+    public void setTankVolume(int tankVolume) {
+        this.tankVolume = tankVolume;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public int getFuelAmount() {
+        return fuelAmount;
+    }
+
+    public int getFuelConsumption() {
+        return fuelConsumption;
+    }
+
+    public int getTankVolume() {
+        return tankVolume;
+    }
+
+    public String getBrake() {
+        return brake;
+    }
+
+    public String getColour() {
+        return colour;
+    }
+
+    public String getSerialNumber() {
+        return serialNumber;
+    }
 }
+
+
