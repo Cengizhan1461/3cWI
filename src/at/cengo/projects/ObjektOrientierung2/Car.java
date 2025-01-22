c588452f8d25780556ea1afd57f064c4b707d325
kasir <cengi@htldornbirn.at>
2025-01-22 09:02:17 +0100

    Aufgabe 11 finished

---
+++ src/at/cengo/projects/ObjektOrientierung2/Car.java
@@ -0,0 +1,108 @@
+package at.cengo.projects.ObjektOrientierung2;
+
+
+//Ein Auto hat eine Farbe, eine Maximalgeschwindigkeit, einen Basispreis,
+//Basisverbrauch und einen Hersteller und einen Motor.
+//Der Hersteller hat einen Namen und ein Herkunftsland.
+//Jeder Hersteller gibt einen gewissen Rabatt. Dieser ist unterschiedlich je nach Hersteller.
+//Der Rabatt wird in Prozent angegeben.
+//Der Motor ist entweder Diesel oder Benzin und hat eine Leistung.
+//Der Preis berechnet sich durch den Abzug des Rabatts vom Basispreis.
+//Der Benzinverbrauch entspricht die ersten 50.000km dem Basisverbrauch.
+// Danach wird er um 9.8 Prozent höher.
+
+
+public class Car {
+    private String colour;
+    private int speed;
+    private int cost;
+    private int fuelConsumption;
+    private Manufacturer manufacturer;
+    private Engine engine;
+
+
+    public Car(String colour, int speed, int cost, int fuelConsumption, Manufacturer manufacturer, Engine engine) {
+        this.colour = colour;
+        this.speed = speed;
+        this.cost = cost;
+        this.fuelConsumption = fuelConsumption;
+        this.manufacturer = manufacturer;
+        this.engine = engine;
+    }
+
+    public String getColour() {
+        return colour;
+    }
+
+    public int getSpeed() {
+        return speed;
+    }
+
+    public int getCost() {
+        return cost;
+    }
+
+    public int getFuelConsumption() {
+        return fuelConsumption;
+    }
+
+    public Manufacturer getManufacturer() {
+        return manufacturer;
+    }
+
+    public Engine getEngine() {
+        return engine;
+    }
+
+    // Setter methods
+    public void setColour(String colour) {
+        this.colour = colour;
+    }
+
+    public void setSpeed(int speed) {
+        this.speed = speed;
+    }
+
+    public void setCost(int cost) {
+        this.cost = cost;
+    }
+
+    public void setFuelConsumption(int fuelConsumption) {
+        this.fuelConsumption = fuelConsumption;
+    }
+
+    public void setManufacturer(Manufacturer manufacturer) {
+        this.manufacturer = manufacturer;
+    }
+
+    public void setEngine(Engine engine) {
+        this.engine = engine;
+    }
+
+    // Calculate the price after applying the discount
+    public int calculatePrice() {
+        int discount = manufacturer.getDiscount();
+        return cost - (cost * discount / 100);
+    }
+
+
+    // Calculate fuel consumption based on kilometers driven
+    public double calculateFuelConsumption(int kilometersDriven) {
+        if (kilometersDriven <= 50000) {
+            return fuelConsumption;
+        } else {
+            return fuelConsumption * 1.098; // Increase by 9.8%
+        }
+    }
+
+
+}
+
+
+
+
+
+
+
+
+
---
+++ src/at/cengo/projects/ObjektOrientierung2/Manufacturer.java
@@ -0,0 +1,43 @@
+package at.cengo.projects.ObjektOrientierung2;
+
+
+//Der Hersteller hat einen Namen und ein Herkunftsland.
+//Jeder Hersteller gibt einen gewissen Rabatt. Dieser ist unterschiedlich je nach Hersteller.
+//Der Rabatt wird in Prozent angegeben.
+public class Manufacturer {
+    private String name;
+    private String country;
+    private int discount;
+
+
+    public Manufacturer(String name, String country, int discount) {
+        this.name = name;
+        this.country = country;
+        this.discount = discount;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public int getDiscount() {
+        return discount;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public void setDiscount(int discount) {
+        this.discount = discount;
+    }
+
+}
